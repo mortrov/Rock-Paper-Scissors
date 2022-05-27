772d8c79af9bd085745e29f9d09a36dc9fcf579a
mortrov <lroemerx90@gmail.com>
2022-05-27 12:51:44 -0700

    moved gameplay function to rpsscripts.js

--- rpsscripts.js
+++ rpsscripts.js
@@ -1,8 +1,21 @@
-
-
 function computerPlay(){
     const rps = ['Rock', 'Paper', 'Scissors'];
     const random = Math.floor(Math.random() * rps.length);
     return rps[random];
 }
 
+function gameround(playerSelection, computerSelection){
+    if(playerSelection == 'rock' && computerSelection == 'paper' ||
+    playerSelection == 'paper' && computerSelection == 'scissors' ||
+    playerSelection == 'scissors' && computerSelection == "rock"){
+        return "You Lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    }
+    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return "Tie!"
+    }
+    else return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+}
+
+function game(){
+
+}
--- second.js
+++ second.js
@@ -1,17 +1,4 @@
-
-function gameround(playerSelection, computerSelection){
-    if(playerSelection == 'rock' && computerSelection == 'paper' ||
-    playerSelection == 'paper' && computerSelection == 'scissors' ||
-    playerSelection == 'scissors' && computerSelection == "rock"){
-        return "You Lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    }
-    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return "Tie!"
-    }
-    else return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-}
-
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-//console.log(playerSelection + ' ' + computerSelection);      selection debugger
+
 console.log(gameround(playerSelection.toLowerCase(),computerSelection.toLowerCase()));
