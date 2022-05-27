a332bb8bf3134273ebf5d6a87ed5e3574d07a535
mortrov <lroemerx90@gmail.com>
2022-05-27 13:29:01 -0700

    added rounds to the game

--- rpsscripts.js
+++ rpsscripts.js
@@ -1,6 +1,6 @@
 function computerPlay(){
     const rps = ['Rock', 'Paper', 'Scissors'];
-    const random = Math.floor(Math.random() * rps.length);
+    let random = Math.floor(Math.random() * rps.length);
     return rps[random];
 }
 
@@ -8,14 +8,17 @@ function gameround(playerSelection, computerSelection){
     if(playerSelection == 'rock' && computerSelection == 'paper' ||
     playerSelection == 'paper' && computerSelection == 'scissors' ||
     playerSelection == 'scissors' && computerSelection == "rock"){
+        compwins++;
         return "You Lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     }
     else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return "Tie!"
+        ties++;
+        return "Tie!";
     }
-    else return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+    else {
+        playerwins++; 
+        return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+       
+    }
 }
 
-function game(){
-
-}
--- second.js
+++ second.js
@@ -1,4 +1,23 @@
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+let playerSelection = "Rock";
+let computerSelection = computerPlay();
+let compwins = 0;
+let playerwins = 0;
+let ties = 0;
 
-console.log(gameround(playerSelection.toLowerCase(),computerSelection.toLowerCase()));
+function game(){
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Pick: Rock, Paper, or Scissors (default: Rock): ");
+        computerSelection = computerPlay();
+        console.log(gameround(playerSelection.toLowerCase(), computerSelection.toLowerCase()));
+        
+    }
+    if(compwins > playerwins){
+        return "You Lose! Computer Score: " + compwins + " | Player score: " + playerwins + " | Ties: " + ties;  
+    }
+    else if(playerwins > compwins){
+        return "Yow Win! Player Score: " + playerwins + " | Computer score: " + compwins + " | Ties: " + ties;
+    }
+    else return "Tie"
+}
+
+console.log(game());
