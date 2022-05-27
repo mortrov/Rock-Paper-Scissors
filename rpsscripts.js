00a2ede1013542df5dc19f4b82bd9b4f527584b8
mortrov <lroemerx90@gmail.com>
2022-05-27 13:32:20 -0700

    moved functions to rpsscrpits.js

--- rpsscripts.js
+++ rpsscripts.js
@@ -22,3 +22,18 @@ function gameround(playerSelection, computerSelection){
     }
 }
 
+function game(){
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Pick: Rock, Paper, or Scissors (default: Rock): ");
+        computerSelection = computerPlay();
+        console.log(gameround(playerSelection.toLowerCase(), computerSelection.toLowerCase()));
+        
+    }
+    if(compwins > playerwins){
+        return "You Lose! Computer Score: " + compwins + " | Player score: " + playerwins + " | Ties: " + ties;  
+    }
+    else if(playerwins > compwins){
+        return "Yow Win! Player Score: " + playerwins + " | Computer score: " + compwins + " | Ties: " + ties;
+    }
+    else return "Tie"
+}
