8431cb0d087b53b38bb2880b8104e3d870388ea4
mortrov <lroemerx90@gmail.com>
2022-05-27 12:47:41 -0700

    cleaned up gameplay code

--- second.js
+++ second.js
@@ -1,16 +1,17 @@
+
 function gameround(playerSelection, computerSelection){
-    if(playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper' ||
-    playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors' ||
-    playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == "rock"){
-        return "You Lose! Paper beats Rock."
+    if(playerSelection == 'rock' && computerSelection == 'paper' ||
+    playerSelection == 'paper' && computerSelection == 'scissors' ||
+    playerSelection == 'scissors' && computerSelection == "rock"){
+        return "You Lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     }
     else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         return "Tie!"
     }
-    else return "You Win!"
+    else return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
 }
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 //console.log(playerSelection + ' ' + computerSelection);      selection debugger
-console.log(gameround(playerSelection,computerSelection));
+console.log(gameround(playerSelection.toLowerCase(),computerSelection.toLowerCase()));
